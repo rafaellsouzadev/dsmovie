2c2828f8d5b79fa86097242fad75ebf4e1a44130
Rafael de Souza Alves <rafaellsouza.java@gmail.com>
2022-03-10 08:58:12 -0300

    MovieCard

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -6,7 +6,7 @@ function NavBar() {
         <header>
         <nav className="container">
           <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
+            <h1>DSMovie</h1>            
             <a href="https://github.com/rafaellsouzadev" target="_blank" rel="noreferrer">
               <div className="dsmovie-contact-container">
                   <GithubIcon />
